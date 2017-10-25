817b530fb221d293b6bed8ad2cf653ec951d46a4
Dalibor Karlović <dalibor.karlovic@burza.hr>
2017-10-25 16:30:37 +0200

    WIP

--- src/Elasticsearch/Extension/SearchExtension.php
+++ src/Elasticsearch/Extension/SearchExtension.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace Rollerworks\Component\Search\ApiPlatform\Elasticsearch\Extension;
 
+use Elastica\Client;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use Doctrine\ORM\QueryBuilder;
+use Elastica\Document;
+use Elastica\Search;
+use Ramsey\Uuid\Uuid;
+use Rollerworks\Component\Search\ApiPlatform\ArrayKeysValidator;
 use Rollerworks\Component\Search\Elasticsearch\ElasticsearchFactory;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -16,16 +21,65 @@ use Symfony\Component\HttpFoundation\RequestStack;
 class SearchExtension implements QueryCollectionExtensionInterface
 {
     private $requestStack;
-    private $ormFactory;
+    private $elasticsearchFactory;
+    private $elasticaClient;
 
-    public function __construct(RequestStack $requestStack, ElasticsearchFactory $ormFactory)
+    public function __construct(RequestStack $requestStack, ElasticsearchFactory $elasticsearchFactory, Client $elasticaClient)
     {
         $this->requestStack = $requestStack;
-        $this->ormFactory = $ormFactory;
+        $this->elasticsearchFactory = $elasticsearchFactory;
+        $this->elasticaClient = $elasticaClient;
+
     }
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
-        throw new \Exception('Not implemented yet');
+        $request = $this->requestStack->getCurrentRequest();
+
+        /** @var SearchCondition $condition */
+        if (!$request || null === $condition = $request->attributes->get('_api_search_condition')) {
+            return;
+        }
+
+        $context = $request->attributes->get('_api_search_context');
+        $configuration = $request->attributes->get('_api_search_config');
+        $configPath = "{$resourceClass}#attributes[rollerworks_search][contexts][{$context}][elasticsearch]";
+
+        if (empty($configuration['elasticsearch'])) {
+            return;
+        }
+
+        $configuration = (array) $configuration['elasticsearch'];
+        ArrayKeysValidator::assertOnlyKeys($configuration, ['mappings'], $configPath);
+
+        $ids =
+
+        // this snippet looks weird, factory should create a
+        $conditionGenerator = $this->elasticsearchFactory->createCachedConditionGenerator(
+            $this->elasticsearchFactory->createConditionGenerator($condition)
+        );
+
+        foreach ($configuration['mappings'] as $fieldName => $mapping) {
+            $conditionGenerator->registerField($fieldName, $mapping);
+        }
+
+        // TODO: temporary, how to do this better?
+        $identifier = 'id';
+        $search = new Search($this->elasticaClient);
+        $response = $search->search($conditionGenerator->getQuery());
+        $identifierValues = array_map(function(Document $document) {
+            return $document->getId();
+        }, $response->getDocuments());
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+
+        // straight from FOS Elastica Bundle
+        $queryBuilder
+            ->andWhere(
+                $queryBuilder
+                    ->expr()
+                        ->in($rootAlias.'.'.$identifier, ':ids')
+            )
+            ->setParameter('ids', $identifierValues);
     }
 }
