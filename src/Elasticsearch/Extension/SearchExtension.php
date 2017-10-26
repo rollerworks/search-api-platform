ee8001cb19067104e23e5906f2e06596eb7bedeb
Dalibor Karlović <dalibor.karlovic@burza.hr>
2017-10-26 15:27:28 +0200

    WIP

--- src/Elasticsearch/Extension/SearchExtension.php
+++ src/Elasticsearch/Extension/SearchExtension.php
@@ -16,11 +16,13 @@ namespace Rollerworks\Component\Search\ApiPlatform\Elasticsearch\Extension;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Elastica\Client;
 use Elastica\Document;
 use Elastica\Search;
 use Rollerworks\Component\Search\ApiPlatform\ArrayKeysValidator;
 use Rollerworks\Component\Search\Elasticsearch\ElasticsearchFactory;
+use Rollerworks\Component\Search\Exception\BadMethodCallException;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
@@ -31,10 +33,12 @@ class SearchExtension implements QueryCollectionExtensionInterface
     private $requestStack;
     private $elasticsearchFactory;
     private $elasticaClient;
+    private $identifierNames = [];
 
-    public function __construct(RequestStack $requestStack, ElasticsearchFactory $elasticsearchFactory, Client $elasticaClient)
+    public function __construct(RequestStack $requestStack, ManagerRegistry $registry, ElasticsearchFactory $elasticsearchFactory, Client $elasticaClient)
     {
         $this->requestStack = $requestStack;
+        $this->registry = $registry;
         $this->elasticsearchFactory = $elasticsearchFactory;
         $this->elasticaClient = $elasticaClient;
     }
@@ -57,7 +61,7 @@ class SearchExtension implements QueryCollectionExtensionInterface
         }
 
         $configuration = (array) $configuration['elasticsearch'];
-        ArrayKeysValidator::assertOnlyKeys($configuration, ['mappings'], $configPath);
+        ArrayKeysValidator::assertOnlyKeys($configuration, ['mappings', 'identifier', 'identifiers_normalizer'], $configPath);
 
         // this snippet looks weird, factory should create a
         $conditionGenerator = $this->elasticsearchFactory->createCachedConditionGenerator(
@@ -68,23 +72,54 @@ class SearchExtension implements QueryCollectionExtensionInterface
             $conditionGenerator->registerField($fieldName, $mapping);
         }
 
+        $normalizer = null;
+        if (array_key_exists('identifiers_normalizer', $configuration)) {
+            $normalizer = $configuration['identifiers_normalizer'];
+            if (!is_callable($normalizer)) {
+                throw new BadMethodCallException('Parameter "identifiers_normalizer" must be a valid callable');
+            }
+        }
+
         // TODO: temporary, how to do this better?
-        $identifier = 'id';
         $search = new Search($this->elasticaClient);
         $response = $search->search($conditionGenerator->getQuery());
-        $identifierValues = array_map(function (Document $document) {
-            return $document->getId();
-        }, $response->getDocuments());
 
-        $rootAlias = $queryBuilder->getRootAliases()[0];
+        // NOTE: written like this so we only check if we have a normalizer once
+        if (null !== $normalizer) {
+            $callable = function (Document $document) use ($normalizer) {
+                return call_user_func($normalizer, $document->getId());
+            };
+        } else {
+            $callable = function (Document $document) {
+                return $document->getId();
+            };
+        }
+        $ids = array_map($callable, $response->getDocuments());
 
         // straight from FOS Elastica Bundle
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $identifier = $this->getIdentifierNames($resourceClass);
+
+        // TODO: hack, only works for non-composite PKs
+        $identifier = current($identifier);
         $queryBuilder
             ->andWhere(
                 $queryBuilder
                     ->expr()
                         ->in($rootAlias.'.'.$identifier, ':ids')
             )
-            ->setParameter('ids', $identifierValues);
+            ->setParameter('ids', $ids);
+    }
+
+    private function getIdentifierNames(string $class): array
+    {
+        if (!array_key_exists($class, $this->identifierNames)) {
+            $manager = $this->registry->getManagerForClass($class);
+            $metadata = $manager->getClassMetadata($class);
+
+            $this->identifierNames[$class] = $metadata->getIdentifier();
+        }
+
+        return $this->identifierNames[$class];
     }
 }
